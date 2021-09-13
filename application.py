a87b8e2e63b71b4e8681abf4a3dce0d4fe18a5a8
dtour <dhirajtourani@Dhirajs-MacBook-Pro.local>
2021-09-13 10:41:34 +0800

    added registration and login forms

--- application.py
+++ application.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask, flash, redirect, render_template, url_for, request, session
 from flask_session import Session
+from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -9,6 +11,8 @@ from datetime import datetime
 
 # Configure application
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'cf43cc3cd14c6194695d8bbc492db6c5'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hotw.db'
 
 # Ensure responses aren't cached
 @app.after_request
@@ -51,9 +55,26 @@ def help():#todo
 def contact():#todo
     return render_template('contact.html')
 
-@app.route('/sign_in')
+@app.route('/register', methods=['GET','POST'])
+def register():#todo
+    form = RegistrationForm()
+
+    # Check if form was valid
+    if form.validate_on_submit():
+        flash(f'Account created for {form.email.data}!', category='success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/sign_in', methods=['GET','POST'])
 def sign_in():#todo
-    return render_template('sign_in.html')
+    form = LoginForm()
+
+    # Check if form was valid
+    if form.validate_on_submit():
+        if form.email.data == 'xx' and form.email.password == 'xx':
+            return redirect(url_for('home'))
+
+    return render_template('sign_in.html', form=form)
 
 @app.route('/sign_out')
 def sign_out():#todo
@@ -63,6 +84,9 @@ def sign_out():#todo
 def account():#todo
     return render_template('account.html')
 
-@app.route('/change_password')
+@app.route('/change_password', methods=['GET','POST'])
 def change_password():#todo
     return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
