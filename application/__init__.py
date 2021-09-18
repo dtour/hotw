fa886837c2da8b0903027930d578d4d92819ca05
Dhiraj Tourani <dtour@hotw.app>
2021-09-18 15:00:08 +0800

    updated config vars

--- application/__init__.py
+++ application/__init__.py
@@ -9,7 +9,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Configure application
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'cf43cc3cd14c6194695d8bbc492db6c5'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '').replace(
         'postgres://', 'postgresql://') or \
         'sqlite:///' + os.path.join(basedir, 'hotw.db')
@@ -19,13 +19,5 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'sign_in'
 login_manager.login_message_category = 'info'
-
-# Ensure responses aren't cached
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
 
 from application import routes
